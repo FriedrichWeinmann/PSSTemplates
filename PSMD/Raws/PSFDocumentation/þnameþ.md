9fe1c42a3f2541fe20ee9a98967fdb7fb09b7157
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2019-01-13 08:48:36 +0100

    updated templates

---
+++ PSMD/Raws/PSFDocumentation/þnameþ.md
@@ -0,0 +1,22 @@
+---
+title: þcategoryþ: þnameþ
+---
+# þnameþ
+## Synopsis
+
+<Insert short synopsis>
+
+## Description
+
+<Insert comprehensive description>
+
+## Notes
+<Update Link>
+[Back to þcategoryþ](http://psframework.org/documentation/documents/þ{
+    $modules = 'PSFramework','PSModuleDevelopment','PSUtil'
+    $modules | Out-GridView -Title 'Select module the documentation is designed for' -OutputMode Single
+}þ/þ{ $Parameters.Category.ToLower() }þ.html)
+
+| Version | 1.0 |
+| Written on: | þ{ Get-Date -Format "yyyy-MM-dd" }þ |
+| Updated on: | þ{ Get-Date -Format "yyyy-MM-dd" }þ |
---
+++ Tools/BuildPSFCache.ps1
@@ -0,0 +1,97 @@
+function New-PSSModuleCache
+{
+<#
+	.SYNOPSIS
+		Calculates cache data used by PowerShell Studio for the specified module.
+	
+	.DESCRIPTION
+		Calculates cache data used by PowerShell Studio for the specified module.
+		This can be used to manually calculate the cache if automatic calculation fails.
+	
+	.PARAMETER Module
+		The module to build the cache for
+	
+	.EXAMPLE
+		PS C:\> Get-Module PSFramework | New-PSSModuleCache
+	
+		Creates PSS cache data for the PSFramework module
+#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[System.Management.Automation.PSModuleInfo[]]
+		$Module
+	)
+	
+	begin
+	{
+		$common = 'Verbose', 'Debug', 'ErrorAction', 'WarningAction', 'InformationAction', 'ErrorVariable', 'WarningVariable', 'InformationVariable', 'OutVariable', 'OutBuffer', 'PipelineVariable'
+	}
+	process
+	{
+		foreach ($moduleItem in $Module)
+		{
+			$ctext = @()
+			$mtext = '{0}|{1}|' -f $moduleItem.Name, $moduleItem.Version.ToString()
+			$atext = @()
+			
+			# Write Header
+			$ctext += '[Module]'
+			$ctext += '{0}|{1}|{2}' -f $moduleItem.Name, $moduleItem.Version.ToString(), $moduleItem.Description
+			
+			$commands = $moduleItem.ExportedCommands.Values
+			foreach ($command in $commands)
+			{
+				if ($command.CommandType -like "Alias")
+				{
+					$atext += '{0} {1}' -f $command.Name, $command.ResolvedCommand.Name
+					continue
+				}
+				$help = Get-Help -Name $command.Name
+				$ctext += '[Command]'
+				$ctext += '{0}|{1}|C' -f $command.CommandType, $command.Name
+				$ctext += ''
+				$ctext += $help.Synopsis
+				$ctext += '[Syntax]'
+				foreach ($set in $command.ParameterSets)
+				{
+					$ctext += '{0}|{1} {2}' -f $set.Name, $command.Name, $set.ToString()
+				}
+				$ctext += '[Outputs]'
+				foreach ($set in $command.ParameterSets)
+				{
+					if ($command.OutputType)
+					{
+						$ctext += '{0}|{1}' -f $set.Name, $command.OutputType[0].Type.ToString()
+					}
+					else { $ctext += '{0}|System.Management.Automation.PSObject[]' -f $set.Name }
+				}
+				$ctext += '[Parameters]'
+				foreach ($parameter in $command.Parameters.Values)
+				{
+					if ($parameter.Name -in $common) { continue }
+					$ctext += '{0}|{1}|' -f $parameter.Name, $parameter.ParameterType
+				}
+			}
+			
+			[pscustomobject]@{
+				PSTypeName = 'PSS.Module.Cache'
+				Module	   = $mtext
+				Aliases    = $atext -join "`n"
+				Commands   = $ctext -join "`n"
+			}
+		}
+	}
+}
+
+$modules = 'PSFramework', 'PSUtil', 'PSModuleDevelopment', 'PSJukebox'
+foreach ($module in $modules)
+{
+	Import-Module $module
+	$cacheData = Get-Module $module | New-PSSModuleCache
+	$moduleObject = Get-Module $module
+	$baseName = 'C:\ProgramData\SAPIEN\PresetCache 2.1\ModuleCacheV5\{0}.{1}' -f $moduleObject.Name, $moduleObject.Version
+	
+	$cacheData.Commands | Set-Content -Path "$baseName.cmdlets"
+	$cacheData.Aliases | Set-Content -Path "$baseName.alias"
+}
