b59933c7407a4051fa211b4f2f262ef6dd7a0ab4
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2022-02-24 13:09:21 +0100

    updates

---
+++ Templates/Comment Help/test.ps1
@@ -0,0 +1,21 @@
+﻿<#
+	.SYNOPSIS
+		Ensure kitten health.
+	
+	.DESCRIPTION
+		Ensure kitten health.
+	
+	.PARAMETER
+		The server hosting the kittenz
+	
+	.NOTES
+		{
+		"Name": "UPDATEME"
+		"Version": "1.0.0",
+		"Author": "Fred",
+		"Company": "Microsoft",
+		"CreatedOn": "2021-02-13",
+		"Tags": [ "kittenz", "furball", "meow" ]
+		}
+#>
+
---
+++ Templates/File Templates/PowerShell/psmd-action.ps1
@@ -0,0 +1,22 @@
+﻿$action = {
+    param (
+        $Parameters
+    )
+	
+    $rootPath = $Parameters.RootPath
+    $actualParameters = $Parameters.Parameters
+	
+    #TODO: Implement Action
+}
+
+$params = @{
+    Name        = '%FileTitle%'
+    Action      = $action
+    Description = 'Does Something'
+    Parameters  = @{
+        Param1 = '(mandatory) Whatever the first parameter does.'
+        Param2 = 'Whatever the second parameter does.'
+    }
+}
+
+Register-PSMDBuildAction @params
